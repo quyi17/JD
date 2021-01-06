8d316a0c3f31621212a8b9df748d9032499109e7
quyi17 <73599301+quyi17@users.noreply.github.com>
2021-01-06 17:16:40 +0800

    Update jd_syj.js

--- jd_syj.js
+++ jd_syj.js
@@ -2,7 +2,7 @@
  * @Author: lxk0301 https://github.com/lxk0301
  * @Date: 2020-11-27 09:19:21
  * @Last Modified by: lxk0301
- * @Last Modified time: 2020-11-27 09:58:02
+ * @Last Modified time: 2021-1-1 16:58:02
  */
 /*
 十元街脚本，一周签到下来可获得30京豆，一天任意时刻运行一次即可
@@ -14,14 +14,11 @@
 [task_local]
 #十元街
 10 7 * * * https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_syj.js, tag=十元街, img-url=https://raw.githubusercontent.com/58xinian/icon/master/jd_syj.png, enabled=true
-
 ================Loon==============
 [Script]
 cron "10 7 * * *" script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_syj.js, tag=十元街
-
 ===============Surge=================
 十元街 = type=cron,cronexp="10 7 * * *",wake-system=1,timeout=20,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_syj.js
-
 ============小火箭=========
 十元街 = type=cron,script-path=https://raw.githubusercontent.com/lxk0301/jd_scripts/master/jd_syj.js, cronexpr="10 7 * * *", timeout=200, enable=true
  */
@@ -46,6 +43,7 @@ if ($.isNode()) {
   cookiesArr.reverse();
   cookiesArr.push(...[$.getdata('CookieJD2'), $.getdata('CookieJD')]);
   cookiesArr.reverse();
+  cookiesArr = cookiesArr.filter(item => item !== "" && item !== null && item !== undefined);
 }
 const JD_API_HOST = 'https://api.m.jd.com/api';
 !(async () => {
@@ -68,8 +66,6 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
 
         if ($.isNode()) {
           await notify.sendNotify(`${$.name}cookie已失效 - ${$.UserName}`, `京东账号${$.index} ${$.UserName}\n请重新登录获取cookie`);
-        } else {
-          $.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。$.setdata('', `CookieJD${i ? i + 1 : "" }`);//cookie失效，故清空cookie。
         }
         continue
       }
@@ -94,7 +90,7 @@ function showMsg() {
 let signFlag = 0;
 function userSignIn() {
   return new Promise(resolve => {
-    const body = {"activityId":"8d6845fe2e77425c82d5078d314d33c5","inviterId":"VMIQlLQqjQyjZokQmv5bIDgq011L0Ov8","channel":"MiniProgram"};
+    const body = {"activityId":"ccd8067defcd4787871b7f0c96fcbf5c","inviterId":"","channel":"MiniProgram"};
     $.get(taskUrl('userSignIn', body), async (err, resp, data) => {
       try {
         if (err) {
@@ -123,6 +119,10 @@ function userSignIn() {
                 signFlag ++;
                 await userSignIn();
               }
+            } else if (data.code === 66) {
+              //此处有时会遇到 服务器繁忙 导致签到失败,故重复三次签到
+              $.log(`${$.name}签到失败:${data.msg}`);
+              message += `【签到】失败，${data.msg}`;
             } else {
               console.log(`异常：${JSON.stringify(data)}`)
             }
